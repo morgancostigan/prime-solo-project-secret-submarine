6b63cd9c99e6dd1fc9fd0a5000e3356f336e8d0d
Morgan Costigan <morgan.costigan@gmail.com>
2018-12-19 15:23:51 -0600

    hashed and salted

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    let queryText = (`SELECT * FROM "secret" 
+        WHERE ($1) > "secret"."secrecy_level";`);
+    pool.query(queryText, [req.user.clearance_level]).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
+
